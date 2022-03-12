b3467d8b552ece1f2ac57f87bc99d6be12d64dd1
a <a@b.com>
2022-03-12 17:23:36 +0300

    Both algorithms are done

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,38 +3,65 @@
 
 #include <iostream>
 #include <conio.h>
+#include <math.h>
 
 #pragma warning(disable: 6031)
 
-bool EventHappens(double a, int R)
+bool EventHappens(double prob)
 {
-    //if (!(a > 0.0) && !(a < 0.0))
-    if (a > 100.0 || a < 0.0)
+    size_t range = 100;
+    if (prob > 100.0 || prob < 0.0)
         throw "Invalid probability";
-    if(R > 100 || R < 1)
-        throw "Invalid input number";
-    if (a == 100.0)
+    if (prob == 100.0)
         return true;
-    if (a == 0.0)
+    if (prob == 0.0)
         return false;
+    while (prob - floor(prob) != 0.0)
+    {
+        range *= 10;
+        prob *= 10.0;
+    }
+    prob *= prob;
+    return !((int)prob % (1 + rand() % range));
+}
 
-    return true;
+bool E(double prob)
+{
+    size_t range = 100;
+    if (prob > 100.0 || prob < 0.0)
+        throw "Invalid probability";
+    if (prob == 100.0)
+        return true;
+    if (prob == 0.0)
+        return false;
+    while (prob - floor(prob) != 0.0)
+    {
+        range *= 10;
+        prob *= 10.0;
+    }
+    return ((double)(1 + rand() % range)) <= prob;
 }
 
 int main()
 {
     srand(time(NULL));
-    int R = 1 + rand() % 100;
-    double a = 13.0;
-    try
+    bool res1, res2;
+    double a = 50.0;
+    for (size_t i = 0; i < 10; i++)
     {
-        bool res = EventHappens(a, R);
+        try
+        {
+            res1 = EventHappens(a);
+            res2 = E(a);
+        }
+        catch (const char* msg)
+        {
+            std::cout << std::string(msg) << std::endl;
+            _getch();
+            return 1;
+        }
+        std::cout << (res1 ? "Event 1 happened" : "Event 1 didn't happen") << ' ' << (res2 ? "Event 2 happened" : "Event 2 didn't happen") << std::endl;
     }
-    catch (const char* msg)
-    {
-        std::cout << std::string(msg) << std::endl;
-        _getch();
-        return 1;
-    }
+    _getch();
     return 0;
 }
